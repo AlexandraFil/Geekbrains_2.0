f3ad9c15f6f5a1b8445e390aa7975c77ffe7a167
Alexandra Panfilova <panfilova.a.d@gmail.com>
2020-06-10 21:45:01 +0300

    csv base for leroymerlin

--- data_mining/lesson_6/leroymerlin/pipelines.py
+++ data_mining/lesson_6/leroymerlin/pipelines.py
@@ -9,6 +9,8 @@ import scrapy
 from scrapy.pipelines.images import ImagesPipeline
 from config import CLIENT_DB
 
+import csv
+
 class LeroymerlinPipeline:
     def process_item(self, item, spider):
         collection = CLIENT_DB[spider.name]
@@ -27,4 +29,25 @@ class LeroymerlinPhotosPipeline(ImagesPipeline):
     def item_completed(self, results, item, info):
         if results:
             item['photos'] = [itm[1] for itm in results if itm[0]]
+        return item
+
+class CSVPipeline():
+    def __init__(self):
+        self.file = f'database.csv'
+        with open(self.file, 'r', newline='') as csv_file:
+            self.tmp_data = csv.DictReader(csv_file).fieldnames
+
+        self.csv_file = open(self.file, 'a', newline='', encoding='UTF-8')
+
+    def __del__(self):
+        self.csv_file.close()
+
+    def process_item(self, item, spider):
+        columns = item.fields.keys()
+
+        data = csv.DictWriter(self.csv_file, columns)
+        if not self.tmp_data:
+            data.writeheader()
+            self.tmp_data = True
+        data.writerow(item)
         return item
--- data_mining/lesson_6/leroymerlin/spiders/leroymerlin_parse.py
+++ data_mining/lesson_6/leroymerlin/spiders/leroymerlin_parse.py
@@ -8,15 +8,6 @@ class LeroymerlinParseSpider(scrapy.Spider):
     allowed_domains = ['leroymerlin.ru']
     start_urls = ['https://leroymerlin.ru/catalogue/podvesnye-svetilniki/']
 
-    # def get_definition(self, response):
-    #     definition_term = response.xpath('//dl[contains(@class, "def-list")]//dt[@class="def-list__term"]/text()').extract()
-    #     definition_def = response.xpath('//dl[contains(@class, "def-list")]//dd[@class="def-list__definition"]/text()').extract()
-    #     for i in range(len(definition_def)):
-    #         definition_def[i] = ' '.join(definition_def[i].split())
-    #
-    #     definition = dict(zip(definition_term, definition_def))
-    #
-    #     return definition
 
     xpt_str = {
         'name' : '//h1/text()',
@@ -29,7 +20,7 @@ class LeroymerlinParseSpider(scrapy.Spider):
         super().__init__()
 
     def parse(self, response):
-        print(1)
+
         pagination = response.xpath('//div[contains(@class, "list-paginator")]//a[contains(@class, "paginator-item")]/@href')
         for link in pagination:
             yield response.follow(link, callback=self.parse)
@@ -53,21 +44,14 @@ class LeroymerlinParseSpider(scrapy.Spider):
             definition_def[i] = ' '.join(definition_def[i].split())
 
         definition = dict(zip(definition_term, definition_def))
+        float_list = ['(мм)', '(кг)', '(в м²)', '(см)', '(лет)']
+        int_list = ['(в K)', '(Вт)', '(В)']
         for key in definition.keys():
-            if '(мм)' in key or '(кг)' in key:
+            if key in float_list:
                 definition[key] = float(definition[key])
-            if '(в K)' in key or '(Вт)' in key:
+            if key in int_list:
                 definition[key] = int(definition[key])
 
         item.add_value('definition', definition)
-
-        print(1)
-
-        # item = LeroymerlinItem(
-        #     name = response.xpath('//h1/text()').extract_first(),
-        #     photos = response.xpath('//picture[@slot="pictures"]//img/@src').extract(),
-        #     price = response.xpath('//span[@slot="price"]/text()').extract_first(),
-        #     definition=definition,
-        # )
 
         yield item.load_item()
